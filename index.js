463ef3e658ee97756d7aea72e2bcb2c96cc12838
Navdeep Singh <navdeep.er@gmail.com>
2020-05-05 12:03:14 +0530

    Update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var https = require('https')
 require('dotenv').config();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(app.get('port'), function() {
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
-});
+})
 
 require('./matches.js')(app);
 require('./bot.js')(app);
